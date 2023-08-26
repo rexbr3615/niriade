aea35f6fc93e7fe0d03b2c835f960c616d2e29b5
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2023-08-26 20:42:00 -0300

    niriadephp

---
+++ components/head.inc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<header class="header">
+    <div class="header-container">
+        <a href="#" class="logo">Niriade1</a>
+        <nav class="nav-menu">
+            <a href=".html">????</a>
+            
+            
+            <div class="language">
+                <div class="language-selected">
+                    pt-BR
+                </div>
+                <ul>
+                    <li><a href="#" class="">Creatures</a></li> <!-- pt-BR -->
+                    <li><a href="#" class="">Structures</a></li> <!-- pt-BR -->
+                    <li><a href="#" class="">World</a></li> <!-- pt-BR -->
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" href="__niriade.png.ico">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/website/css/uicons-outline-rounded.css"rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Niriade</title>
+    <script src="general.js"></script>
+</head>
+
+
+
+<body>
+
+    <?php include('components/head.inc.php'); ?>
+
+    <p>Oque é Niriade?</p>
+    <p>Niriade é um projeto meu de planeta e fauna ficticios</p>
+
+    <a href="">fauna roadrunner</a>
+    <a href="">fauna frost stalker</a>
+    <a href="">fauna condor solitario</a>
+    <a href="">plant nauron</a>
+    <a href="">fauna Gryllidae minimus</a>
+    <a href="">fauna Axolotls</a>
+    <a href="">fishe Ghost shark</a>
+    <a href="">fauna Apple boa</a>
+    <a href="">fungi slatas slenos</a>
+    <a href="">fauna corujinha setentrional</a>
+    <a href="">fauna falcão grilloeiro</a>
+    <a href="">fauna slimes</a>
+    <a href="">fauna stalker</a>
+
+    <a href="">raça Xolotl</a>
+
+    <a href="">magic: Elementos/magia</a>
+    <a href="">magic: ley lines</a>
+    <a href="">magic: obelisk</a>
+    <a href="">magic: espiritos</a>
+    <a href="">magic: espiritos remanescentes</a>
+    <a href="">magic: entidades</a>
+    <a href="">magic: deuses</a>
+    <a href="">magic: primordiais</a>
+    <a href="">magic: ego</a>
+    <a href="">flux</a>
+    <a href="">alma</a>
+
+
+    <br>
+    <br>
+    <p>1</p>
+    <p>2</p>
+         
+
+    <script src="general.js"></script>
+    
+</body>
+</html>
+
+    
+
